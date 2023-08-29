3c659789ad0f2930a7d3905a3758cade3efd6b93
wegner01 <kenneth.lola119@est.unanleon.edu.ni>
2023-08-29 10:02:25 -0600

    Added Comment Controller

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using P4TableRelations.Models;
+
+namespace P4TableRelations.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly SocialMediaContext _context;
+
+        public CommentController(SocialMediaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comment
+        public async Task<IActionResult> Index()
+        {
+            var socialMediaContext = _context.Comments.Include(c => c.Post);
+            return View(await socialMediaContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Save(Comment model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.Date = DateTime.Now.Date.ToShortDateString();
+                _context.Comments.Add(model);
+                await _context.SaveChangesAsync();
+                return Redirect("/Post/Details/" + model.PostId);
+            }
+            return Redirect("Post/Index");
+        }
+
+
+        // GET: Comment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Comments == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Post)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comment/Create
+        public IActionResult Create()
+        {
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id");
+            return View();
+        }
+
+        // POST: Comment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CommentId,UserName,Date,Text,PostId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", comment.PostId);
+            return View(comment);
+        }
+
+        // GET: Comment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Comments == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", comment.PostId);
+            return View(comment);
+        }
+
+        // POST: Comment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CommentId,UserName,Date,Text,PostId")] Comment comment)
+        {
+            if (id != comment.CommentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.CommentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", comment.PostId);
+            return View(comment);
+        }
+
+        // GET: Comment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Comments == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Post)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Comments == null)
+            {
+                return Problem("Entity set 'SocialMediaContext.Comments'  is null.");
+            }
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommentExists(int id)
+        {
+          return (_context.Comments?.Any(e => e.CommentId == id)).GetValueOrDefault();
+        }
+    }
+}
