38139ab4ecee6832d34b01dc66eeaa4fe22251f5
wegner01 <kenneth.lola119@est.unanleon.edu.ni>
2023-08-28 20:43:45 -0600

    Added SocialMediaContext class definition

--- Models/SocialMediaContext.cs
+++ Models/SocialMediaContext.cs
@@ -1,9 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace P4TableRelations.Models {
-    public class SocialMediaContext : DbContext {
-        public SocialMediaContext(DbContextOptions<SocialMediaContext> options) : base (options) { }
+namespace P4TableRelations.Models;
+public class SocialMediaContext : DbContext {
+    public SocialMediaContext(DbContextOptions<SocialMediaContext> options) : base (options) { }
 
-        
-    }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 }
